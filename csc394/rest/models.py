06e0bb12c25a59d62846dd34b92ac7e175e53ac7
Tom Shepardson <tomshepardson@yahoo.com>
2016-04-17 19:34:17 -0500

    Updated migrations
    you may need to delete your db.sql if you created that.

--- csc394/rest/models.py
+++ csc394/rest/models.py
@@ -1,11 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    address = models.CharField(max_length=50)
+    url = models.URLField(blank=True)
+    size = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class MessageThread(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+class Message(models.Model):
+    thread = models.ForeignKey(MessageThread, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, unique=True)
+
+class ProductGroup(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+
 class Product(models.Model):
-    product_name = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    product_group = models.ForeignKey(ProductGroup)
+    name = models.CharField(max_length=50)
     description = models.TextField()
     cost = models.DecimalField(max_digits=8, decimal_places=2)
     manufacturer_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.product_name
+
+class UserStory(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    description = models.TextField()
 
     def __str__(self):
         return self.product_name

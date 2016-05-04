34776543d51ca09c361d3f0e5f27c10454b18e6d
Tom Shepardson <tomshepardson@yahoo.com>
2016-05-04 17:36:32 -0500

    create teared admin page

--- csc394/rest/admin.py
+++ csc394/rest/admin.py
@@ -2,15 +2,28 @@ from django.contrib import admin
 
 import csc394.rest.models as md
 from django.contrib.admin import AdminSite
+# from django.contrib.contenttypes.admin import TabularInline
 
 class MyAdminSite(AdminSite):
     site_header = 'MedTech Administration'
+
+class ProductFeatureInline(admin.TabularInline):
+    model = md.ProductFeature
+    fields = (('feature', 'description'),)
+
+class ProductAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    inlines = [
+        ProductFeatureInline,
+    ]
+
+    # fields = (('company', 'cost'), 'name')
 
 admin_site = MyAdminSite(name='myadmin')
 
 admin_site.register(md.Company)
 admin_site.register(md.ProductGroup)
-admin_site.register(md.Product)
+admin_site.register(md.Product, ProductAdmin)
 admin_site.register(md.MessageThread)
 admin_site.register(md.UserStory)
 admin_site.register(md.Evaluation)
